1a8de515f2850d44f61a4f4fc8ee7b7324163a6f
Jonah Tech <jonahmunag@gmail.com>
2026-04-22 16:14:29 +0200

    Add files via upload
    Gestion des patients et médicaments dans un hôpital

---
+++ Gestion d'un hopital/app/controllers/MedicamentController.php
@@ -0,0 +1,9 @@
+<?php
+class MedicamentController extends Controller{
+    public function displayMedicament(){
+        $medoc = new Medicament();
+        $data['medocs'] = $medoc->getAllMedicaments();
+
+        $this->view("Medicament", $data['medocs']);
+    }
+}
