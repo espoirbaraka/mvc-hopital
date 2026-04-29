a898695f8085a68002ca17e0e00aeca2591be73e
chance <sylvainchance019@gmail.com>
2026-04-29 13:08:12 +0200

    commentaire comment inserer les médicaments

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ include "../core/Controller.php";
 include "../core/Model.php";
 include "../app/controllers/MedicamentController.php";
 include "../app/models/Medicament.php";
+// comment inserer les médicaments
 
 $medicament = new MedicamentController();
 
